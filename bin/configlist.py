f1a09d87c8a0a9539fe70fa83c3f2cb31db39ac3
Max Isi <max.isi@ligo.org>
2014-11-19 17:17:25 -0600

    configlist: exclude 'all' for C1

--- bin/configlist.py
+++ bin/configlist.py
@@ -20,7 +20,10 @@ elif len(sys.argv)>2:
 config_string = ''
 for dir_name, subdir_list, file_list in os.walk(rootdir):
     for f_name in file_list:
-        rightstart = f_name.lower().startswith(prefix.lower()) or f_name.lower().startswith('all')
+        rightstart = f_name.lower().startswith(prefix.lower())
+        # include files tagged "all", if IFO is not C1
+        if prefix.lower() != 'c1':
+            rightstart = rightsart or f_name.lower().startswith('all')
         if f_name[-3:] == 'ini' and rightstart:
             config_string += ' --config-file ' + os.path.join(dir_name, f_name)
 
